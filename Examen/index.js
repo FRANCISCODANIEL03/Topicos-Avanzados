82ed52b3d9511c638eea11089f392ea123e21599
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-20 18:29:24 -0600

    apply format at document

--- Examen/index.js
+++ Examen/index.js
@@ -6,24 +6,24 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.listen(4000,()=>console.log("Servidor creado en el puerto 4000"));
+app.listen(4000, () => console.log("Servidor creado en el puerto 4000"));
 
 let listT = [];
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json(listT);
 });
-app.post("/",(req,res)=>{
-    const {tarea} = req.body;
-    if(tarea == ""){
+app.post("/", (req, res) => {
+    const { tarea } = req.body;
+    if (tarea == "") {
         return res.json({
             msg: "Tarea vacia",
             value: 2
         });
-    }else{
-    listT.push(tarea);
-    res.status(200).json({
-        msg: "Tarea agregada correctamente",
-        value: 1
-    });
+    } else {
+        listT.push(tarea);
+        res.status(200).json({
+            msg: "Tarea agregada correctamente",
+            value: 1
+        });
     }
 });
