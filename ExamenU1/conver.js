d0f98623469d269098a855ba4f5b0c6765f2ff9c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-21 18:29:59 -0600

    apply format at document

--- ExamenU1/conver.js
+++ ExamenU1/conver.js
@@ -7,35 +7,35 @@ let pul = document.getElementById("in");
 let pie = document.getElementById("ft");
 let yar = document.getElementById("yd");
 
-const calcular =(e)=>{
+const calcular = (e) => {
     e.preventDefault();
-    let valor =document.getElementById("val").value;
-    let milimetro,kilometro,pulgada,pies,yarda,metro;
-    
-    metro = parseFloat(valor /100);
-    milimetro = parseFloat(metro*1000);
+    let valor = document.getElementById("val").value;
+    let milimetro, kilometro, pulgada, pies, yarda, metro;
+
+    metro = parseFloat(valor / 100);
+    milimetro = parseFloat(metro * 1000);
     kilometro = parseFloat(metro * .001).toFixed(4);
     pulgada = parseFloat(metro * 39.3701).toFixed(4);
     pies = parseFloat(metro * 3.28084).toFixed(4);
     yarda = parseFloat(metro * 1.09361).toFixed(4);
 
-    mil.textContent="el valor en milimetros es " + milimetro;
-    met.textContent="el valor en metros es " + metro;
-    kil.textContent="el valor en kilometros es " + kilometro;
-    pul.textContent="el valor en pulgadas es " + pulgada;
-    pie.textContent="el valor en pies es " + pies;
-    yar.textContent="el valor en yardas es " + yarda;
+    mil.textContent = "el valor en milimetros es " + milimetro;
+    met.textContent = "el valor en metros es " + metro;
+    kil.textContent = "el valor en kilometros es " + kilometro;
+    pul.textContent = "el valor en pulgadas es " + pulgada;
+    pie.textContent = "el valor en pies es " + pies;
+    yar.textContent = "el valor en yardas es " + yarda;
 }
-const limpiar =(e)=>{
+const limpiar = (e) => {
     e.preventDefault();
-    let valor =document.getElementById("val");
-    valor.value ="";
-    mil.textContent="";
-    met.textContent="";
-    kil.textContent="";
-    pul.textContent="";
-    pie.textContent="";
-    yar.textContent="";
+    let valor = document.getElementById("val");
+    valor.value = "";
+    mil.textContent = "";
+    met.textContent = "";
+    kil.textContent = "";
+    pul.textContent = "";
+    pie.textContent = "";
+    yar.textContent = "";
 }
-Conv.addEventListener("submit",calcular);
-Conv.addEventListener("reset",limpiar);
+Conv.addEventListener("submit", calcular);
+Conv.addEventListener("reset", limpiar);
