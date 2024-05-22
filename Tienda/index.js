41c64e1e06e62956211c20e55c6d42c0a5c838f1
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-21 18:32:05 -0600

    apply format at document

--- Tienda/index.js
+++ Tienda/index.js
@@ -6,56 +6,56 @@ const app = express();
 app.use(cors());//para recibir datos de ips distintas a localhost 
 
 app.use(express.json());//permite leer los datos JSON   
-app.listen(3000,()=>console.log("Servidor creado en el puerto 3000"));
+app.listen(3000, () => console.log("Servidor creado en el puerto 3000"));
 
-app.get("/producto",(req,res)=>{
+app.get("/producto", (req, res) => {
     const lista = {
-        productos:[
-        {
-            Nombre:"Aceite",
-            precio:50,
-            marca:"Nutrioli"
-        },
-        {
-            Nombre:"Jabon de polvo",
-            precio:35,
-            marca:"Roma"
-        },
-        {
-            Nombre:"Galletas",
-            precio:25,
-            marca:"Gamesa"
-        },
-        {
-            Nombre:"Papas fritas",
-            precio:20,
-            marca:"Sabritas"
-        },
-        {
-            Nombre:"Azucar",
-            precio:35,
-            marca:"Morena"
-        },
-        {
-            Nombre:"Sal",
-            precio:30,
-            marca:"La Fina"
-        }
-    ]
+        productos: [
+            {
+                Nombre: "Aceite",
+                precio: 50,
+                marca: "Nutrioli"
+            },
+            {
+                Nombre: "Jabon de polvo",
+                precio: 35,
+                marca: "Roma"
+            },
+            {
+                Nombre: "Galletas",
+                precio: 25,
+                marca: "Gamesa"
+            },
+            {
+                Nombre: "Papas fritas",
+                precio: 20,
+                marca: "Sabritas"
+            },
+            {
+                Nombre: "Azucar",
+                precio: 35,
+                marca: "Morena"
+            },
+            {
+                Nombre: "Sal",
+                precio: 30,
+                marca: "La Fina"
+            }
+        ]
     }
     res.json(lista);
 })
-const product=[]
-let productsName=[]
-let productsPrice=[]
-app.post("/compra", (req, res)=>{
+const product = []
+let productsName = []
+let productsPrice = []
+app.post("/compra", (req, res) => {
     const data = req.body;
-    let total= 0;
-    for(let i=0; i<data.length; i++){
-        product[i]= data[i].split(' $');
-        productsName= [product[i][0], ...productsName]
-        productsPrice= [product[i][1], ...productsPrice]
-        total+=parseFloat(productsPrice);
+    let total = 0;
+    for (let i = 0; i < data.length; i++) {
+        product[i] = data[i].split(' $');
+        productsName = [product[i][0], ...productsName]
+        productsPrice = [product[i][1], ...productsPrice]
+        total += parseFloat(productsPrice);
     }
 
     res.send(`El total de tu compra es: $ ${total}`)
